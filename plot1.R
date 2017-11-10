01237a0cd5dc2c02925ca79729069aa15f796576
Kunal Chowdhury <kunal.chowdhury@gmail.com>
2017-11-11 02:05:30 +0800

    Added plot 1

---
+++ plot1.R
@@ -0,0 +1,16 @@
+# Read dataset lines 66637 to 69516 for data of days 01/02/2007 - 02/02/2007
+header <- read.table("household_power_consumption.txt", nrows=1, header=F,
+                     sep=';', stringsAsFactors=F)
+powerdata <- read.table("household_power_consumption.txt", sep=";",
+                        na.strings=c("?", ""), skip=66636, nrows=2880)
+colnames(powerdata) <- header
+powerdata$TimeStamp <- strptime(paste(powerdata$Date, powerdata$Time),
+                                "%d/%m/%Y %H:%M:%S", tz="GMT")
+
+# Start png graphic device
+png(filename="plot1.png")
+
+# histogram of "global active power"
+hist(powerdata$Global_active_power, xlab="Global Active Power (kilowatts)",
+     main="Global Active Power", col="red")
+dev.off()
